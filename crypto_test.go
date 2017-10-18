30130c3a73d2aa2990576249256dfd9f014b63aa
Zack Scholl <zack.scholl@gmail.com>
2017-10-18 10:29:41 -0600

    Fix tests
    Former-commit-id: 7ce613531c92f9f8a6d132e4cb80341cd6a5de52

--- crypto_test.go
+++ crypto_test.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestEncrypt(t *testing.T) {
 	key := GetRandomName()
-	fmt.Println(key)
-	salt, iv, encrypted := Encrypt([]byte("hello, world"), key)
-	fmt.Println(len(encrypted))
-	decrypted, err := Decrypt(salt, iv, encrypted, key)
+	encrypted, salt, iv := Encrypt([]byte("hello, world"), key)
+	decrypted, err := Decrypt(encrypted, key, salt, iv)
 	if err != nil {
 		t.Error(err)
 	}
 	if string(decrypted) != "hello, world" {
 		t.Error("problem decrypting")
 	}
-	_, err = Decrypt(salt, iv, encrypted, "wrong passphrase")
+	_, err = Decrypt(encrypted, "wrong passphrase", salt, iv)
 	if err == nil {
 		t.Error("should not work!")
 	}
