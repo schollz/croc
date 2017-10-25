4802e03d3575b168889c78fdbdaf9342b311dbf2
h3yEllex <artiomfotescu1@gmail.com>
2017-10-25 14:49:31 +0300

    Tests for FileSize
    Former-commit-id: fe9ccb8677919ce2d6f41616ce18c23801a5c8e0

--- utils_test.go
+++ utils_test.go
@@ -6,10 +6,28 @@ import (
 )
 
 func TestSplitFile(t *testing.T) {
-	err := SplitFile("README.md", 3)
+	err := SplitFile("testing_data/README.md", 3)
 	if err != nil {
 		t.Error(err)
 	}
-	os.Remove("README.md.0")
-	os.Remove("README.md.1")
+	os.Remove("testing_data/README.md.0")
+	os.Remove("testing_data/README.md.1")
+}
+
+func TestFileSize(t *testing.T) {
+	t.Run("File is ok ", func(t *testing.T) {
+		_, err := FileSize("testing_data/README.md")
+		if err != nil {
+			t.Errorf("should pass with no error, got: %v", err)
+		}
+	})
+	t.Run("File does not exist", func(t *testing.T) {
+		s, err := FileSize("testing_data/someStrangeFile")
+		if err == nil {
+			t.Error("should return an error")
+		}
+		if s > 0 {
+			t.Errorf("size should be 0, got: %d", s)
+		}
+	})
 }
