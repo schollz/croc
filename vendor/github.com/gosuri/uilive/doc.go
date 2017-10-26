4d0cd8298dbf6e1e70e06905f2384b99f4673909
Zack Scholl <zack.scholl@gmail.com>
2017-10-26 12:36:17 -0600

    Updated dependencies
    Former-commit-id: e24d37db7dffaa300a3a64f6c24f0353b16f1c59

--- vendor/github.com/gosuri/uilive/doc.go
+++
@@ -1,2 +0,0 @@
-// Package uilive provides a writer that live updates the terminal. It provides a buffered io.Writer that is flushed at a timed interval.
-package uilive
--- vendor/github.com/gosuri/uiprogress/doc.go
+++
@@ -1,2 +0,0 @@
-// Package uiprogress is a library to render progress bars in terminal applications
-package uiprogress
