61a3b45d69882828bb87cd18f92129ffe48b4b72
smileboywtu <smileboywtu@gmail.com>
2018-02-09 10:17:25 +0800

    add home dir config .croc
    Former-commit-id: 1eb07db618a9258c437c1a76fe0d0895600b9014

---
+++ help.go
@@ -0,0 +1,19 @@
+package main
+
+var helpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} [options]
+
+VERSION:
+   {{.Version}}{{if or .Author .Email}}
+
+AUTHOR:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+
+OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}
+`
