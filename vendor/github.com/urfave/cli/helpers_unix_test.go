f0ed13c392588430f70df144c494e1e049c7bc67
Zack Scholl <zack.scholl@gmail.com>
2018-04-22 21:41:44 -0700

    delete vendor for working

--- vendor/github.com/urfave/cli/helpers_unix_test.go
+++
@@ -1,9 +0,0 @@
-// +build darwin dragonfly freebsd linux netbsd openbsd solaris
-
-package cli
-
-import "os"
-
-func clearenv() {
-	os.Clearenv()
-}
--- vendor/github.com/yudai/gotty/server/init_message.go
+++
@@ -1,6 +0,0 @@
-package server
-
-type InitMessage struct {
-	Arguments string `json:"Arguments,omitempty"`
-	AuthToken string `json:"AuthToken,omitempty"`
-}
