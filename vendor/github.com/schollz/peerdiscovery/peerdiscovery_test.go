346284cbed29d15e906589f8d4e75600c0b97c90
Zack Scholl <zack.scholl@gmail.com>
2018-04-24 20:58:41 -0700

    update dependencies

--- vendor/github.com/schollz/peerdiscovery/peerdiscovery_test.go
+++ vendor/github.com/schollz/peerdiscovery/peerdiscovery_test.go
@@ -7,34 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSettings(t *testing.T) {
-	_, err := New()
-	assert.Nil(t, err)
-
-	_, err = New(Settings{
-		Limit:     -1,
-		Payload:   []byte("payload"),
-		Delay:     500 * time.Millisecond,
-		TimeLimit: 10 * time.Second,
-	})
-	assert.Nil(t, err)
-
-	_, err = New(Settings{
-		MulticastAddress: "assd.asdf.asdf.asfd",
-	})
-	assert.NotNil(t, err)
-}
-
 func TestDiscovery(t *testing.T) {
-	p, _ := New(Settings{
+	// should not be able to "discover" itself
+	discoveries, err := Discover(Settings{
 		Limit:     -1,
 		Payload:   []byte("payload"),
 		Delay:     500 * time.Millisecond,
 		TimeLimit: 5 * time.Second,
 	})
-
-	// should not be able to "discover" itself
-	discoveries, err := p.Discover()
 	assert.Nil(t, err)
 	assert.Zero(t, len(discoveries))
 }
