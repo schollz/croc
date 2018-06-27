44c08329fbb1dbc56c5d0a59e7e498ea93f6dec6
Zack Scholl <zack.scholl@gmail.com>
2018-06-26 22:04:45 -0600

    vendor

--- vendor/github.com/briandowns/spinner/spinner.go
+++ vendor/github.com/briandowns/spinner/spinner.go
@@ -21,6 +21,7 @@ import (
 	"sync"
 	"time"
 	"unicode/utf8"
+	"encoding/hex"
 
 	"github.com/fatih/color"
 )
@@ -30,6 +31,8 @@ var errInvalidColor = errors.New("invalid color")
 
 // validColors holds an array of the only colors allowed
 var validColors = map[string]bool{
+	// default colors for backwards compatibility
+	"black":   true,
 	"red":     true,
 	"green":   true,
 	"yellow":  true,
@@ -37,10 +40,64 @@ var validColors = map[string]bool{
 	"magenta": true,
 	"cyan":    true,
 	"white":   true,
+
+	// attributes
+	"reset":        true,
+	"bold":         true,
+	"faint":        true,
+	"italic":       true,
+	"underline":    true,
+	"blinkslow":    true,
+	"blinkrapid":   true,
+	"reversevideo": true,
+	"concealed":    true,
+	"crossedout":   true,
+
+	// foreground text
+	"fgBlack":   true,
+	"fgRed":     true,
+	"fgGreen":   true,
+	"fgYellow":  true,
+	"fgBlue":    true,
+	"fgMagenta": true,
+	"fgCyan":    true,
+	"fgWhite":   true,
+
+	// foreground Hi-Intensity text
+	"fgHiBlack":   true,
+	"fgHiRed":     true,
+	"fgHiGreen":   true,
+	"fgHiYellow":  true,
+	"fgHiBlue":    true,
+	"fgHiMagenta": true,
+	"fgHiCyan":    true,
+	"fgHiWhite":   true,
+
+	// background text
+	"bgBlack":   true,
+	"bgRed":     true,
+	"bgGreen":   true,
+	"bgYellow":  true,
+	"bgBlue":    true,
+	"bgMagenta": true,
+	"bgCyan":    true,
+	"bgWhite":   true,
+
+	// background Hi-Intensity text
+	"bgHiBlack":   true,
+	"bgHiRed":     true,
+	"bgHiGreen":   true,
+	"bgHiYellow":  true,
+	"bgHiBlue":    true,
+	"bgHiMagenta": true,
+	"bgHiCyan":    true,
+	"bgHiWhite":   true,
 }
 
 // returns a valid color's foreground text color attribute
 var colorAttributeMap = map[string]color.Attribute{
+	// default colors for backwards compatibility
+	"black":   color.FgBlack,
 	"red":     color.FgRed,
 	"green":   color.FgGreen,
 	"yellow":  color.FgYellow,
@@ -48,6 +105,58 @@ var colorAttributeMap = map[string]color.Attribute{
 	"magenta": color.FgMagenta,
 	"cyan":    color.FgCyan,
 	"white":   color.FgWhite,
+
+	// attributes
+	"reset":        color.Reset,
+	"bold":         color.Bold,
+	"faint":        color.Faint,
+	"italic":       color.Italic,
+	"underline":    color.Underline,
+	"blinkslow":    color.BlinkSlow,
+	"blinkrapid":   color.BlinkRapid,
+	"reversevideo": color.ReverseVideo,
+	"concealed":    color.Concealed,
+	"crossedout":   color.CrossedOut,
+
+	// foreground text colors
+	"fgBlack":   color.FgBlack,
+	"fgRed":     color.FgRed,
+	"fgGreen":   color.FgGreen,
+	"fgYellow":  color.FgYellow,
+	"fgBlue":    color.FgBlue,
+	"fgMagenta": color.FgMagenta,
+	"fgCyan":    color.FgCyan,
+	"fgWhite":   color.FgWhite,
+
+	// foreground Hi-Intensity text colors
+	"fgHiBlack":   color.FgHiBlack,
+	"fgHiRed":     color.FgHiRed,
+	"fgHiGreen":   color.FgHiGreen,
+	"fgHiYellow":  color.FgHiYellow,
+	"fgHiBlue":    color.FgHiBlue,
+	"fgHiMagenta": color.FgHiMagenta,
+	"fgHiCyan":    color.FgHiCyan,
+	"fgHiWhite":   color.FgHiWhite,
+
+	// background text colors
+	"bgBlack":   color.BgBlack,
+	"bgRed":     color.BgRed,
+	"bgGreen":   color.BgGreen,
+	"bgYellow":  color.BgYellow,
+	"bgBlue":    color.BgBlue,
+	"bgMagenta": color.BgMagenta,
+	"bgCyan":    color.BgCyan,
+	"bgWhite":   color.BgWhite,
+
+	// background Hi-Intensity text colors
+	"bgHiBlack":   color.BgHiBlack,
+	"bgHiRed":     color.BgHiRed,
+	"bgHiGreen":   color.BgHiGreen,
+	"bgHiYellow":  color.BgHiYellow,
+	"bgHiBlue":    color.BgHiBlue,
+	"bgHiMagenta": color.BgHiMagenta,
+	"bgHiCyan":    color.BgHiCyan,
+	"bgHiWhite":   color.BgHiWhite,
 }
 
 // validColor will make sure the given color is actually allowed
@@ -85,6 +194,11 @@ func New(cs []string, d time.Duration) *Spinner {
 		active:   false,
 		stopChan: make(chan struct{}, 1),
 	}
+}
+
+// Active will return whether or not the spinner is currently active
+func (s *Spinner) Active() bool {
+	return s.active
 }
 
 // Start will start the indicator
@@ -147,11 +261,20 @@ func (s *Spinner) Reverse() {
 }
 
 // Color will set the struct field for the given color to be used
-func (s *Spinner) Color(c string) error {
-	if !validColor(c) {
-		return errInvalidColor
+func (s *Spinner) Color(colors ...string) error {
+
+	colorAttributes := make([]color.Attribute, len(colors))
+
+	// Verify colours are valid and place the appropriate attribute in the array
+	for index, c := range colors {
+		if !validColor(c) {
+			return errInvalidColor
+		}
+
+		colorAttributes[index] = colorAttributeMap[c]
 	}
-	s.color = color.New(colorAttributeMap[c]).SprintFunc()
+
+	s.color = color.New(colorAttributes...).SprintFunc()
 	s.Restart()
 	return nil
 }
@@ -175,7 +298,8 @@ func (s *Spinner) UpdateCharSet(cs []string) {
 // Caller must already hold s.lock.
 func (s *Spinner) erase() {
 	n := utf8.RuneCountInString(s.lastOutput)
-	for _, c := range []string{"\b", " ", "\b"} {
+	del, _ := hex.DecodeString("7f")
+	for _, c := range []string{"\b", string(del), "\b"} {
 		for i := 0; i < n; i++ {
 			fmt.Fprintf(s.Writer, c)
 		}
--- vendor/github.com/yudai/gotty/backend/localcommand/doc.go
+++
@@ -1,3 +0,0 @@
-// Package localcommand provides an implementation of webtty.Slave
-// that launches a local command with a PTY.
-package localcommand
