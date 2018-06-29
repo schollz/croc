0746faea68214777226d22028e2e5b761faf9634
Zack Scholl <zack.scholl@gmail.com>
2018-06-29 06:47:43 -0700

    update the secret variables

--- src/api.go
+++ src/api.go
@@ -1,23 +1,5 @@
 package croc
 
-import "time"
-
-// Init will initialize the croc relay
-func Init() (c *Croc) {
-	c = new(Croc)
-	c.TcpPorts = []string{"27001", "27002", "27003", "27004"}
-	c.ServerPort = "8003"
-	c.Timeout = 10 * time.Minute
-	c.UseEncryption = true
-	c.UseCompression = true
-	c.AllowLocalDiscovery = true
-	c.CurveType = "p521"
-	c.rs.Lock()
-	c.rs.channel = make(map[string]*channelData)
-	c.rs.Unlock()
-	return
-}
-
 // Relay initiates a relay
 func (c *Croc) Relay() error {
 	// start relay
