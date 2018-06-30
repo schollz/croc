68d5a13236ecd550deec67d17f425d6979110920
Zack Scholl <zack.scholl@gmail.com>
2018-06-29 19:34:21 -0700

    pake works between two clients
    Former-commit-id: 72e2d4d3d8da53566c09ec3816aeba8a27172a63

--- main.go
+++ main.go
@@ -9,15 +9,16 @@ import (
 func main() {
 	var err error
 	role := flag.Int("role", 0, "role number")
+	passphrase := flag.String("code", "chou", "codephrase")
 	flag.Parse()
 
 	c := croc.Init()
 	if *role == -1 {
 		err = c.Relay()
 	} else if *role == 0 {
-		err = c.Send("foo")
+		err = c.Send("foo", *passphrase)
 	} else {
-		err = c.Receive()
+		err = c.Receive(*passphrase)
 	}
 	if err != nil {
 		panic(err)
