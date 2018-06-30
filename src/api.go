e6b55ebdb39628cb381f8c1e41fd1fc217381d0b
Zack Scholl <zack.scholl@gmail.com>
2018-06-30 10:56:42 -0700

    add peer discovery

--- src/api.go
+++ src/api.go
@@ -1,5 +1,13 @@
 package croc
 
+import (
+	"net"
+	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/schollz/peerdiscovery"
+)
+
 func init() {
 	SetLogLevel("debug")
 }
@@ -15,10 +23,40 @@ func (c *Croc) Relay() error {
 
 // Send will take an existing file or folder and send it through the croc relay
 func (c *Croc) Send(fname string, codephrase string) (err error) {
+	// start relay for listening
+	c.TcpPorts = []string{"27140,27141"}
+	c.ServerPort = "8140"
+	go c.Relay()
+
+	// start client
 	return c.client(0, codephrase, fname)
 }
 
 // Receive will receive something through the croc relay
 func (c *Croc) Receive(codephrase string) (err error) {
+	// try to discovery codephrase and server through peer network
+	discovered, errDiscover := peerdiscovery.Discover(peerdiscovery.Settings{
+		Limit:     1,
+		TimeLimit: 1 * time.Second,
+		Delay:     50 * time.Millisecond,
+		Payload:   []byte(codephrase),
+	})
+	if errDiscover != nil {
+		log.Debug(errDiscover)
+	}
+	if len(discovered) > 0 {
+		log.Debugf("discovered %s on %s", discovered[0].Payload, discovered[0].Address)
+		_, connectTimeout := net.DialTimeout("tcp", discovered[0].Address+":27001", 1*time.Second)
+		if connectTimeout == nil {
+			log.Debug("connected")
+			c.WebsocketAddress = "ws://" + discovered[0].Address + ":8140"
+			log.Debug(discovered[0].Address)
+			codephrase = string(discovered[0].Payload)
+
+		}
+	} else {
+		log.Debug("discovered no peers")
+	}
+
 	return c.client(1, codephrase)
 }
