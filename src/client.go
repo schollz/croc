c2abe365890e74d9c2d177b1e0b2923f4db11f3e
Zack Scholl <zack.scholl@gmail.com>
2018-06-30 11:45:30 -0700

    fix bug

--- src/client.go
+++ src/client.go
@@ -17,79 +17,20 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
 	"github.com/schollz/croc/src/pake"
-	"github.com/schollz/peerdiscovery"
 	"github.com/schollz/progressbar"
 )
 
-func (c *Croc) client(role int, codePhrase string, fname ...string) (err error) {
+var isPrinted bool
+
+func (c *Croc) client(role int, channel string) (err error) {
 	defer log.Flush()
 	defer c.cleanup()
 	// initialize the channel data for this client
-	c.cs.Lock()
-	c.cs.channel.codePhrase = codePhrase
-	if len(codePhrase) > 0 {
-		if len(codePhrase) < 4 {
-			err = errors.New("code phrase must be more than 4 characters")
-			c.cs.Unlock()
-			return
-		}
-		c.cs.channel.Channel = codePhrase[:3]
-		c.cs.channel.passPhrase = codePhrase[3:]
-	} else {
-		// TODO
-		if role == 0 {
-			// generate code phrase
-			codePhrase = getRandomName()
-			dash := strings.Index(codePhrase, "-")
-			c.cs.channel.Channel = codePhrase[:dash]
-			c.cs.channel.passPhrase = codePhrase[dash:]
-		} else {
-			codePhrase = promptCodePhrase()
-			if len(codePhrase) < 4 {
-				err = errors.New("code phrase must be more than 4 characters")
-				c.cs.Unlock()
-				return
-			}
-			c.cs.channel.Channel = codePhrase[:3]
-			c.cs.channel.passPhrase = codePhrase[3:]
-		}
-		c.cs.channel.codePhrase = codePhrase
-	}
-	log.Debugf("codephrase: '%s'", codePhrase)
-	log.Debugf("channel: '%s'", c.cs.channel.Channel)
-	log.Debugf("passPhrase: '%s'", c.cs.channel.passPhrase)
-	channel := c.cs.channel.Channel
-	c.cs.Unlock()
-
-	if role == 0 {
-		// start peer discovery
-		go func() {
-			log.Debug("listening for local croc relay...")
-			go peerdiscovery.Discover(peerdiscovery.Settings{
-				Limit:     1,
-				TimeLimit: 600 * time.Second,
-				Delay:     50 * time.Millisecond,
-				Payload:   []byte(codePhrase),
-			})
-		}()
-
-		if len(fname) == 0 {
-			err = errors.New("must include filename")
-			return
-		}
-		err = c.processFile(fname[0])
-		if err != nil {
-			return
-		}
-	}
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	// connect to the websocket
-	// TODO:
-	// use predefined host and HTTPS, if exists
-
 	u := url.URL{Scheme: strings.Split(c.WebsocketAddress, "://")[0], Host: strings.Split(c.WebsocketAddress, "://")[1], Path: "/"}
 	log.Debugf("connecting to %s", u.String())
 	ws, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
