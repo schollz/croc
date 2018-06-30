f1b62ea30dd56a56ed4f0637824d9a1d1c144792
Zack Scholl <zack.scholl@gmail.com>
2018-06-30 09:01:30 -0700

    check if file exists

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	croc "github.com/schollz/croc/src"
 )
@@ -10,17 +11,20 @@ func main() {
 	var err error
 	role := flag.Int("role", 0, "role number")
 	passphrase := flag.String("code", "chou", "codephrase")
+	fname := flag.String("file", "", "codephrase")
 	flag.Parse()
 
 	c := croc.Init()
+	// croc.SetLogLevel("error")
 	if *role == -1 {
 		err = c.Relay()
 	} else if *role == 0 {
-		err = c.Send("README.md", *passphrase)
+		err = c.Send(*fname, *passphrase)
 	} else {
 		err = c.Receive(*passphrase)
 	}
 	if err != nil {
-		panic(err)
+		fmt.Print("Error: ")
+		fmt.Println(err.Error())
 	}
 }
