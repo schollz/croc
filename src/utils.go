2ac6a6413f2a18bd60b998f6718d8081095f2509
Zack Scholl <zack.scholl@gmail.com>
2018-06-30 05:29:22 -0700

    add zip file
    Former-commit-id: b30523f0978318244ffc37765e4d5d68c81989f1

--- src/utils.go
+++ src/utils.go
@@ -192,3 +192,13 @@ func getLocalIP() string {
 	}
 	return bestIP
 }
+
+// exists reports whether the named file or directory exists.
+func exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
