38f615a6222368cccb3d89d7b860a26b987bb5bb
Zack Scholl <zack.scholl@gmail.com>
2018-06-30 10:45:23 -0700

    add basic ui

--- src/cleanup.go
+++ src/cleanup.go
@@ -1,6 +1,26 @@
 package croc
 
+import (
+	"os"
+	"strconv"
+)
+
 func (c *Croc) cleanup() {
-	// TODO
 	// erase all the croc files and their possible numbers
+	for i := 0; i < 100; i++ {
+		fname := c.crocFile + "." + strconv.Itoa(i)
+		if !exists(fname) {
+			break
+		}
+		os.Remove(fname)
+	}
+	for i := 0; i < 100; i++ {
+		fname := c.crocFileEncrypted + "." + strconv.Itoa(i)
+		if !exists(fname) {
+			break
+		}
+		os.Remove(fname)
+	}
+	os.Remove(c.crocFile)
+	os.Remove(c.crocFileEncrypted)
 }
