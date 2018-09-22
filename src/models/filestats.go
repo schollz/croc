5cb1568737ada8aa5cef3b599088639c39acfeec
Zack Scholl <zack.scholl@gmail.com>
2018-09-22 10:44:16 -0700

    add is compressed/encrypted to filestats
    Former-commit-id: f977ac24c40bd56562450db873721d9b03c0cac9

--- src/models/filestats.go
+++ src/models/filestats.go
@@ -2,10 +2,13 @@ package models
 
 import "time"
 
+// FileStats are the file stats transfered to the other
 type FileStats struct {
-	Name     string
-	Size     int64
-	ModTime  time.Time
-	IsDir    bool
-	SentName string
+	Name         string
+	Size         int64
+	ModTime      time.Time
+	IsDir        bool
+	SentName     string
+	IsCompressed bool
+	IsEncrypted  bool
 }
