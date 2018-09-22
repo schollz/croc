4ac538e0b24509dbbfe43c8129a52969d1c29f39
Zack Scholl <zack.scholl@gmail.com>
2018-09-22 07:15:43 -0700

    increase buffer
    Former-commit-id: 54b22cd732d9d4f27c626d50e2169ca5fc518d3e

--- src/relay/conn.go
+++ src/relay/conn.go
@@ -19,12 +19,12 @@ const (
 	pingPeriod = (pongWait * 9) / 10
 
 	// Maximum message size allowed from peer.
-	maxMessageSize = 1024 * 1024
+	maxMessageSize = 1024 * 1024 * 16
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024 * 1024,
-	WriteBufferSize: 1024 * 1024,
+	ReadBufferSize:  1024 * 1024 * 8,
+	WriteBufferSize: 1024 * 1024 * 8,
 }
 
 // connection is an middleman between the websocket connection and the hub.
