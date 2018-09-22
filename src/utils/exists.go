3a4d231ed24d181d0c94f7c8daf37a20e2f8dac5
Zack Scholl <zack.scholl@gmail.com>
2018-09-21 21:56:47 -0700

    add exists
    Former-commit-id: a225a08dbc07912558304121ecf367d866d4f687

---
+++ src/utils/exists.go
@@ -0,0 +1,13 @@
+package utils
+
+import "os"
+
+// Exists reports whether the named file or directory exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
