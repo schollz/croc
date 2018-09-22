caf551180534b7fdf1f6fc77b6da1bfa230cda04
Zack Scholl <zack.scholl@gmail.com>
2018-09-21 21:50:20 -0700

    add zipping

---
+++ src/zipper/zip_test.go
@@ -0,0 +1,23 @@
+package zipper
+
+import (
+	"os"
+	"testing"
+
+	log "github.com/cihub/seelog"
+	"github.com/schollz/croc/src/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestZip(t *testing.T) {
+	defer log.Flush()
+	DebugLevel = "debug"
+	writtenFilename, err := ZipFile("../../README.md", false)
+	assert.Nil(t, err)
+	defer os.Remove(writtenFilename)
+
+	err = UnzipFile(writtenFilename, ".")
+	assert.Nil(t, err)
+	assert.True(t, utils.Exists("README.md"))
+	os.RemoveAll("README.md")
+}
