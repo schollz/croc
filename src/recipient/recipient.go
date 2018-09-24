c8582ba7de036f3ebaf3c5bcac7dea1917d760ff
Zack Scholl <zack.scholl@gmail.com>
2018-09-24 16:05:34 -0700

    make sure to close connection
    Former-commit-id: 7df06272b85d062e2059af114c969ddf81ed174f

--- src/recipient/recipient.go
+++ src/recipient/recipient.go
@@ -329,6 +329,7 @@ func connectToTCPServer(room string, address string) (com comm.Comm, err error) 
 	if err != nil {
 		return
 	}
+	defer connection.Close()
 	connection.SetReadDeadline(time.Now().Add(3 * time.Hour))
 	connection.SetDeadline(time.Now().Add(3 * time.Hour))
 	connection.SetWriteDeadline(time.Now().Add(3 * time.Hour))
