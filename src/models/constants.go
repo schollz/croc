a878fb1c57244434c1d50ba869927862dc46f3b8
Zack Scholl <zack.scholl@gmail.com>
2018-09-25 18:56:36 -0700

    don't need this anymore
    Former-commit-id: 162b12f42b0e25c539e97e36d6f9bb60c31de1e3

--- src/models/constants.go
+++ src/models/constants.go
@@ -2,8 +2,3 @@ package models
 
 const WEBSOCKET_BUFFER_SIZE = 1024 * 1024 * 32
 const TCP_BUFFER_SIZE = 1024 * 64
-
-type BytesAndLocation struct {
-	Bytes    []byte `json:"b"`
-	Location int64  `json:"l"`
-}
