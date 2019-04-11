c2795104e512674d088c18e74d242be204b923f1
Zack Scholl <zack.scholl@gmail.com>
2019-04-11 12:57:32 -0700

    add bytes

---
+++ src/utils/bytes.go
@@ -0,0 +1,16 @@
+package utils
+
+import "fmt"
+
+func ByteCountDecimal(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+}
