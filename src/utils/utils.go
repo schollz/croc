cee3e866d1e1673d0e356012fb20c7f3f30c9a04
Zack Scholl <zack.scholl@gmail.com>
2019-04-29 17:16:38 -0600

    add bytecountdecimal
    Former-commit-id: 3685a887b89100145520f0858018b551119db608

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -96,3 +96,16 @@ func GetRandomName() string {
 	result = mnemonicode.EncodeWordList(result, bs)
 	return strings.Join(result, "-")
 }
+
+func ByteCountDecimal(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+}
