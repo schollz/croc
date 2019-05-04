26b444d1686e1903af24f5eea6c2f7ece835a2fd
Zack Scholl <zack.scholl@gmail.com>
2019-05-03 20:15:29 -0700

    include hash in version
    Former-commit-id: cb69e492839e540a8c539072d8e1bc73766c5542

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -27,7 +27,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v6.0.0"
+		Version = "v4.1.6-134-ga651297-a651297"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
