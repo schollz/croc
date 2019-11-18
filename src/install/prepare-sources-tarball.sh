45a043dd37fde48345bbe06aed07afb70794462b
Zack Scholl <zack.scholl@gmail.com>
2019-11-18 12:02:54 -0800

    fix src bundle
    Former-commit-id: c369f67745e55795aeba5a93c05a824053ca745a

--- src/install/prepare-sources-tarball.sh
+++ src/install/prepare-sources-tarball.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 tmp=$(mktemp -d)
+VERSION=$(cat ./src/cli/cli.go | grep 'Version = "v' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+echo $VERSION
 git clone --depth 1 https://github.com/schollz/croc $tmp/croc
 (cd $tmp/croc && go mod tidy && go mod vendor)
-(cd $tmp && tar -cvzf croc-src.tar.gz croc)
-mv $tmp/croc-src.tar.gz dist/
+(cd $tmp && tar -cvzf croc_${VERSION}_src.tar.gz croc)
+mv $tmp/croc_${VERSION}_src.tar.gz dist/
