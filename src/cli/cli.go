6a036d68f6bf916e854a9af60f61b859e0a8de94
Zack Scholl <zack.scholl@gmail.com>
2019-11-18 07:29:11 -0800

    update releaser

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -32,7 +32,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v6.2.3-b7625ef"
+		Version = "v6.2.4-63ccb14"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
