85c3ca8846075767d7787bd3cfe165f81fd5655d
Zack Scholl <zack.scholl@gmail.com>
2019-11-19 09:24:13 -0800

    add upload asset to goreleaser #172
    Former-commit-id: 0694d61294015def8df0d2cc75f18d4cdadfcfa8

--- src/install/prepare-sources-tarball.sh
+++ src/install/prepare-sources-tarball.sh
@@ -6,3 +6,51 @@ git clone --depth 1 https://github.com/schollz/croc $tmp/croc
 (cd $tmp/croc && go mod tidy && go mod vendor)
 (cd $tmp && tar -cvzf croc_${VERSION}_src.tar.gz croc)
 mv $tmp/croc_${VERSION}_src.tar.gz dist/
+
+# Check dependencies.
+set -e
+xargs=$(which gxargs || which xargs)
+
+# Validate settings.
+[ "$TRACE" ] && set -x
+
+CONFIG=$@
+
+for line in $CONFIG; do
+  eval "$line"
+done
+
+owner="schollz"
+repo="croc"
+tag="v${VERSION}"
+filename="dist/croc_${VERSION}_src.tar.gz"
+
+# Define variables.
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$owner/$repo"
+GH_TAGS="$GH_REPO/releases/tags/$tag"
+AUTH="Authorization: token $GITHUB_TOKEN"
+WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
+CURL_ARGS="-LJO#"
+
+if [[ "$tag" == 'LATEST' ]]; then
+  GH_TAGS="$GH_REPO/releases/latest"
+fi
+
+# Validate token.
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
+# Read asset tags.
+response=$(curl -sH "$AUTH" $GH_TAGS)
+
+# Get ID of the asset based on given filename.
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+
+# Upload asset
+echo "Uploading asset... "
+
+# Construct url
+GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
+
+curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/octet-stream" $GH_ASSET
