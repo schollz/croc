75688d897d9e90182895b2409b3b6c14bacb9d5e
Zack Scholl <zack.scholl@gmail.com>
2019-11-19 16:04:58 -0800

    remove ping

--- src/croc/croc.go
+++ src/croc/croc.go
@@ -384,7 +384,6 @@ func (c *Client) CreateOfferer(finished chan<- error) (pc *webrtc.PeerConnection
 		return nil
 	}
 
-	var lastSignal = time.Now()
 	var readyToBegin = false
 	var readyToEnd = false
 	var bar *progressbar.ProgressBar
@@ -413,9 +412,8 @@ func (c *Client) CreateOfferer(finished chan<- error) (pc *webrtc.PeerConnection
 			for {
 				for {
 
-					time.Sleep(1 * time.Millisecond)
-					log.Debug(time.Since(lastSignal).Seconds())
-					if readyToBegin && time.Since(lastSignal).Seconds() < 2 {
+					time.Sleep(10 * time.Millisecond)
+					if readyToBegin {
 						break
 					}
 				}
@@ -486,7 +484,6 @@ func (c *Client) CreateOfferer(finished chan<- error) (pc *webrtc.PeerConnection
 			return
 		} else if bytes.Equal(dcMsg.Data, []byte{2, 3, 4}) {
 			// log.Debug("got ready to begin")
-			lastSignal = time.Now()
 			readyToBegin = true
 			return
 		} else if bytes.Equal(dcMsg.Data, []byte{1, 3, 4}) {
@@ -513,10 +510,7 @@ func (c *Client) CreateOfferer(finished chan<- error) (pc *webrtc.PeerConnection
 			n, _ := fwrite.Write(fd.Data)
 			bar.Add(n)
 
-			if time.Since(lastSignal).Seconds() > 1 {
-				sendData([]byte{2, 3, 4})
-				lastSignal = time.Now()
-			}
+			// sendData([]byte{2, 3, 4})
 		} else {
 			log.Error(err)
 		}
