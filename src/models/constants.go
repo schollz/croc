695444159e167a9a2f9a08133df19dfdc6474fd3
Zack Scholl <zack.scholl@gmail.com>
2020-08-27 09:24:19 -0700

    add default ipv6 relay

--- src/models/constants.go
+++ src/models/constants.go
@@ -5,3 +5,4 @@ const TCP_BUFFER_SIZE = 1024 * 64
 
 // DEFAULT_RELAY is the default relay used (can be set using --relay)
 const DEFAULT_RELAY = "142.93.177.120:9009"
+const DEFAULT_RELAY6 = "[2604:a880:800:c1::14c:1]:9009"
