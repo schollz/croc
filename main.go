aac93e6b99ad4d922e27752c781c87bf8f1721d9
Zack Scholl <zack.scholl@gmail.com>
2020-09-03 14:19:32 -0700

    don't tag version before commiting
    Former-commit-id: 3692e32dfb3c144cbd782861dfb86d8ab6fcf268

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-//go:generate git tag -af v$VERSION -m "v$VERSION"
 //go:generate go run src/install/updateversion.go
 //go:generate git commit -am "bump $VERSION"
 //go:generate git tag -af v$VERSION -m "v$VERSION"
