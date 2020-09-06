bdb859c15c4041d30fba66827637f05ed7d9e41d
Zack Scholl <zack.scholl@gmail.com>
2020-09-06 08:45:59 -0700

    use version

--- src/install/prepare-sources-tarball.sh
+++ src/install/prepare-sources-tarball.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 tmp=$(mktemp -d)
-VERSION=$(cat ./src/cli/cli.go | grep 'Version = "v' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
 echo $VERSION
 git clone -b v${VERSION} --depth 1 https://github.com/schollz/croc $tmp/croc-${VERSION}
 (cd $tmp/croc-${VERSION} && go mod tidy && go mod vendor)
