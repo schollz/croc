208d530db74671d312b4864fd49b6872500e929a
Zack Scholl <zack.scholl@gmail.com>
2020-10-13 12:26:11 -0700

    use dns to determine relay
    Former-commit-id: 5374be1a0ff9a55c7bade339964c338116012e51

--- src/models/constants.go
+++ src/models/constants.go
@@ -1,8 +1,22 @@
 package models
 
+import "net"
+
 // TCP_BUFFER_SIZE is the maximum packet size
 const TCP_BUFFER_SIZE = 1024 * 64
 
 // DEFAULT_RELAY is the default relay used (can be set using --relay)
-const DEFAULT_RELAY = "142.93.177.120:9009"
-const DEFAULT_RELAY6 = "[2604:a880:800:c1::14c:1]:9009"
+var DEFAULT_RELAY = "croc.schollz.com"
+var DEFAULT_RELAY6 = "croc6.schollz.com"
+var DEFAULT_PORT = "9009"
+
+func init() {
+	iprecords, _ := net.LookupIP(DEFAULT_RELAY)
+	for _, ip := range iprecords {
+		DEFAULT_RELAY = ip.String() + ":" + DEFAULT_PORT
+	}
+	iprecords, _ = net.LookupIP(DEFAULT_RELAY6)
+	for _, ip := range iprecords {
+		DEFAULT_RELAY6 = "[" + ip.String() + "]:" + DEFAULT_PORT
+	}
+}
