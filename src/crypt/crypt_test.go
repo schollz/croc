420030998fdb74f155aa03156f6ce7524120e4bd
Zack Scholl <zack.scholl@gmail.com>
2021-04-21 16:53:29 -0700

    add chacha20

--- src/crypt/crypt_test.go
+++ src/crypt/crypt_test.go
@@ -1,6 +1,7 @@
 package crypt
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,6 +21,23 @@ func BenchmarkDecrypt(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Decrypt(enc, key)
+	}
+}
+
+func BenchmarkEncryptChaCha(b *testing.B) {
+	bob, _, _ := NewArgon2([]byte("password"), nil)
+	for i := 0; i < b.N; i++ {
+		EncryptChaCha([]byte("hello, world"), bob)
+	}
+}
+
+func BenchmarkDecryptChaCha(b *testing.B) {
+	key, _, _ := NewArgon2([]byte("password"), nil)
+	msg := []byte("hello, world")
+	enc, _ := EncryptChaCha(msg, key)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		DecryptChaCha(enc, key)
 	}
 }
 
@@ -51,5 +69,37 @@ func TestEncryption(t *testing.T) {
 
 	// error with small password
 	_, _, err = New([]byte(""), nil)
+	assert.NotNil(t, err)
+}
+
+func TestEncryptionChaCha(t *testing.T) {
+	key, salt, err := NewArgon2([]byte("password"), nil)
+	fmt.Printf("key: %x\n", key)
+	assert.Nil(t, err)
+	msg := []byte("hello, world")
+	enc, err := EncryptChaCha(msg, key)
+	assert.Nil(t, err)
+	dec, err := DecryptChaCha(enc, key)
+	assert.Nil(t, err)
+	assert.Equal(t, msg, dec)
+
+	// check reusing the salt
+	key2, _, err := NewArgon2([]byte("password"), salt)
+	dec, err = DecryptChaCha(enc, key2)
+	assert.Nil(t, err)
+	assert.Equal(t, msg, dec)
+
+	// check reusing the salt
+	key2, _, err = NewArgon2([]byte("wrong password"), salt)
+	dec, err = DecryptChaCha(enc, key2)
+	assert.NotNil(t, err)
+	assert.NotEqual(t, msg, dec)
+
+	// error with no password
+	dec, err = DecryptChaCha([]byte(""), key)
+	assert.NotNil(t, err)
+
+	// error with small password
+	_, _, err = NewArgon2([]byte(""), nil)
 	assert.NotNil(t, err)
 }
