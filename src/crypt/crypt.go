7c35241ed4e8ba4c8ad9458a7927f78eeca1e52b
Zack Scholl <zack.scholl@gmail.com>
2021-04-21 16:53:29 -0700

    add chacha20
    Former-commit-id: 420030998fdb74f155aa03156f6ce7524120e4bd

--- src/crypt/crypt.go
+++ src/crypt/crypt.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"log"
 
+	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/crypto/pbkdf2"
 )
 
@@ -68,5 +70,64 @@ func Decrypt(encrypted []byte, key []byte) (plaintext []byte, err error) {
 		return
 	}
 	plaintext, err = aesgcm.Open(nil, encrypted[:12], encrypted[12:], nil)
+	return
+}
+
+// NewArgon2 generates a new key based on a passphrase and salt
+// using argon2
+// https://pkg.go.dev/golang.org/x/crypto/argon2
+func NewArgon2(passphrase []byte, usersalt []byte) (key []byte, salt []byte, err error) {
+	if len(passphrase) < 1 {
+		err = fmt.Errorf("need more than that for passphrase")
+		return
+	}
+	if usersalt == nil {
+		salt = make([]byte, 8)
+		// http://www.ietf.org/rfc/rfc2898.txt
+		// Salt.
+		if _, err := rand.Read(salt); err != nil {
+			log.Fatalf("can't get random salt: %v", err)
+		}
+	} else {
+		salt = usersalt
+	}
+	key = argon2.IDKey(passphrase, salt, 1, 64*1024, 4, 32)
+	return
+}
+
+// EncryptChaCha will encrypt ChaCha20-Poly1305 using the pre-generated key
+// https://pkg.go.dev/golang.org/x/crypto/chacha20poly1305
+func EncryptChaCha(plaintext []byte, key []byte) (encrypted []byte, err error) {
+	aead, err := chacha20poly1305.NewX(key)
+	if err != nil {
+		return
+	}
+	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(plaintext)+aead.Overhead())
+	if _, err := rand.Read(nonce); err != nil {
+		panic(err)
+	}
+
+	// Encrypt the message and append the ciphertext to the nonce.
+	encrypted = aead.Seal(nonce, nonce, plaintext, nil)
+	return
+}
+
+// DecryptChaCha will encrypt ChaCha20-Poly1305 using the pre-generated key
+// https://pkg.go.dev/golang.org/x/crypto/chacha20poly1305
+func DecryptChaCha(encryptedMsg []byte, key []byte) (encrypted []byte, err error) {
+	aead, err := chacha20poly1305.NewX(key)
+	if err != nil {
+		return
+	}
+	if len(encryptedMsg) < aead.NonceSize() {
+		err = fmt.Errorf("ciphertext too short")
+		return
+	}
+
+	// Split nonce and ciphertext.
+	nonce, ciphertext := encryptedMsg[:aead.NonceSize()], encryptedMsg[aead.NonceSize():]
+
+	// Decrypt the message and check it wasn't tampered with.
+	encrypted, err = aead.Open(nil, nonce, ciphertext, nil)
 	return
 }
