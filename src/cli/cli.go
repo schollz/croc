04f2dfac975ffa6e3da902c2bed4bde6d5a84a10
Zack Scholl <zack.scholl@gmail.com>
2021-10-02 10:41:46 -0700

    bump 9.4.0

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -34,7 +34,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v9.4.0-78b6f2d"
+		Version = "v9.4.0-060ce51"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
