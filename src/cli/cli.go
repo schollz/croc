eb31d1bc6f9e5ed6fbb604ee63aa4af945a97e6a
Zack Scholl <zack.scholl@gmail.com>
2023-03-14 04:50:10 -0700

    bump 9.6.4
    Former-commit-id: 0656bb78513de1b3112ba1028b11b03522117b22

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -35,7 +35,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v9.6.4-1220d54"
+		Version = "v9.6.4-5ed8204"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
