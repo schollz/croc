1321725012a70e3edd8aaf47aa9d58227288b1ef
Zack <zack.scholl@gmail.com>
2024-09-01 14:38:55 -0700

    10.0.11 -> 10.0.12
    Former-commit-id: a29b8e6d1d7886d48c0e28cbd80df90f3aa67f16

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -36,7 +36,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v10.0.11"
+		Version = "v10.0.12"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
