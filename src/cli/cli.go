7cc18e675a37dbc8a5c2cee1b1c69bd1e5949f3d
Zack Scholl <zack.scholl@gmail.com>
2025-10-16 07:37:56 -0700

    v10.2.6

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -36,7 +36,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v10.2.5"
+		Version = "v10.2.6"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
