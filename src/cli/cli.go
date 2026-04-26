f6025fea72bc3125fc266f5d25539ea008e2d051
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2026-04-26 15:57:41 +0200

    fix: Tell user minimum length of custom code (#1094)
    * Update code phrase min chars in README
    
    * Put min code chars in cli help
    
    Updated usage message for the 'code' flag to specify minimum character requirement.
    
    * Include min code characters in error
    
    Former-commit-id: 11bbae1813df906768d7de39e301eaff65abcd9c

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -69,7 +69,7 @@ func Run() (err error) {
 			ArgsUsage:   "[filename(s) or folder]",
 			Flags: []cli.Flag{
 				&cli.BoolFlag{Name: "zip", Usage: "zip folder before sending"},
-				&cli.StringFlag{Name: "code", Aliases: []string{"c"}, Usage: "codephrase used to connect to relay"},
+				&cli.StringFlag{Name: "code", Aliases: []string{"c"}, Usage: "codephrase used to connect to relay (at least 6 characters)"},
 				&cli.StringFlag{Name: "hash", Value: "xxhash", Usage: "hash algorithm (xxhash, imohash, md5)"},
 				&cli.StringFlag{Name: "text", Aliases: []string{"t"}, Usage: "send some text"},
 				&cli.BoolFlag{Name: "no-local", Usage: "disable local relay when sending"},
